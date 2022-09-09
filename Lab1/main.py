cfa558b87ffa05987473f24bd5e83e8458a288fe
Vladimir <vladimir.parusov.vp1@gmail.com>
2022-09-09 15:29:38 +0300

    Fixed in place

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,6 +1,7 @@
 import math
 
 def task(i):
+    print("")
     print("*** TASK", i)
     return
 
@@ -24,16 +25,23 @@ def normw(x, w):
     
 def main():
     task(2)
-    x, y = [i for i in range(-10, 5)], [i for i in range(-5, 10)]
+    x, y = [i for i in range(-10, 6)], [i for i in range(-5, 11)]
     print("vec x", x)
     print("vec y", y)
     task(3)
     z = [(y[int(i / 2)] if i % 2 == 0 else x[int((i-1) / 2)]) for i in range(len(x) + len(y))]
     print("vec z", z)
     task(4)   
-    print("x norm_1", norm1(x), "norm_2", norm2(x))
-    print("y norm_1", norm1(y), "norm_2", norm2(y))
-    print("z norm_1", norm1(z), "norm_2", norm2(z))
+    example_weight = [float(i) for i in range(len(x))]
+    example_weight = [x / sum(example_weight) for x in example_weight]
+
+    example_weight2 = [float(i) for i in range(len(z))]
+    example_weight2 = [x / sum(example_weight2) for x in example_weight2]
+    print("weights for x and y ", example_weight)
+    print("weights for z ", example_weight2)
+    print("x norm_1", norm1(x), "norm_2", norm2(x), "norm_w", normw(x, example_weight))
+    print("y norm_1", norm1(y), "norm_2", norm2(y), "norm_w", normw(x, example_weight))
+    print("z norm_1", norm1(z), "norm_2", norm2(z), "norm_w", normw(x, example_weight2))
     task(6)
     ans = int(input("Print number to calculate factorial:"))
     print(str(ans) + "! =", math.factorial(ans))
@@ -51,12 +59,13 @@ def main():
     if len(w) != 5:
         print("Not 5 elements provided")
         return
-    if min(w) <= 0:
+    if min(w) < 0:
         print("Weight cannot be negative")
         return
     if sum(w) != 1:
         print("Sum must be equal 1")
-        return
+        w = [float(t) / sum(w) for t in w]
+        #return
     print("norm_w", normw(ans, w))
     
 if __name__ == "__main__":
